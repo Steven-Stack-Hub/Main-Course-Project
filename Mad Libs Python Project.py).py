b3ec185bea165290cf544b1970b242e6a32821fd
Steven Sardar <steven1sardar@gmail.com>
2025-07-13 03:25:40 +0000

    Refactor Mad Libs game code to enhance structure and readability

--- Mad Libs Python Project.py).py
+++ Mad Libs Python Project.py).py
@@ -1,7 +1,50 @@
-Name = "Steven"
-print("Hello, " + "Steven")
-print("Welcome to the Mad Libs game, " + Name + "!")
-print( "this is my updated version!" )
-print("Let's create a fun story together.")
-print("Please provide the following words to fill in the blanks:")
-adjective1 = input("Adjective: ")
+name = "Beaux"; print(name.upper()) # this is an inline comment
+print(type(name)) # this is a function to print the type of the variable
+
+print(isinstance(name, str)) # this is a function to print the type of the variable to see if it is a string
+
+age = float(3.9) # this is a float
+
+print(age) # this is a float
+
+print(isinstance(age, float))
+
+age = 3, 2.5, # This is a Tuple
+
+print(age)
+
+print(type(age)) 
+
+print(isinstance(age, tuple)) # this is a function to print the type of the variable to see if it is a tuple
+
+age = (20) # this is an integer
+
+print(isinstance(age, int)) # this is a function to print the type of the variable to see if it is an integer
+
+#Arithmetic Operators
+print(1 + 1)  # Addition
+print(2 - 1)  # Subtraction
+print(2 * 2)  # Multiplication
+print(9 / 3)  # Division
+print(5 % 2)  # Modulus
+print(2 ** 3) # Exponentiation
+print(5 // 2) # Floor Division
+
+age = 8 
+age += 8
+print(age) # age = age + 8
+
+#Comparison Operators
+a = 5
+b = 10
+
+print(a == b)  # Equal to
+print(a != b)  # Not equal to
+print(a > b)   # Greater than
+print(a < b)   # Less than
+print(a >= b)  # Greater than or equal to
+print(a <= b)  # Less than or equal to
+
+name = "Be\"aux\"au" #This is a Hashmark comment for the term escaping 
+
+print(name.upper()) # this is an inline comment
